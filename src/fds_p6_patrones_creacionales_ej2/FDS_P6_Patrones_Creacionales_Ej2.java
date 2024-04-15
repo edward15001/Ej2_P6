24d97a0b8f43641e67fb758342b50bae54506545
edward15001 <edward.15.holanda@gmail.com>
2024-04-15 16:28:54 +0200

    inicializacion proyecto

---
+++ src/fds_p6_patrones_creacionales_ej2/FDS_P6_Patrones_Creacionales_Ej2.java
@@ -0,0 +1,12 @@
+package fds_p6_patrones_creacionales_ej2;
+
+public class FDS_P6_Patrones_Creacionales_Ej2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+    
+}
